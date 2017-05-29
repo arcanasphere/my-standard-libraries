3d2e0600546cc9c3ede59639f3e3573b50b146d9
arcanasphere <amjohnston@arcanasphere.com>
2017-05-29 11:52:01 -0500

    Add files via upload
    Updated Lightbox Javascript:
    * Now scans all links inside of div.product-row
    * Only opens lightbox if the image ends in gif, jpg, or png
    
    Updates to Lightbox CSS sheet.
    * Slightly fancier caption on top of screen.

--- js/grooming-site-lightbox.js
+++ js/grooming-site-lightbox.js
@@ -1,32 +1,32 @@
-<script>
   var $FadeSpeed = 600;  /* Fade in and out, in milliseconds. Change to make faster or slower */
 
   /* Add the necessary div for the lightbox */
   /* Lightbox will appear on either a link either:
-    1) inside of div.product image
-    2) with class PegBootLightBoxLink
+    1) inside of div.product row, if the link is an image.
+    2) with class PegBootLightBoxLink, if the link is an image
   */
   $(document).ready(function() {
     $("#FullPageContainer").append('<div id="PegBootLightBox"><h2 id="PegBootLightBoxHeadline"></h2></div>');
   });
 
   /* Begin code to find & show the lightbox + image when clicked */
-  $("#FullPageContainer").on("click", ".product-image a, a.PegBootLightBoxLink", function(e) {
+  $("#FullPageContainer").on("click", ".product-row a, a.PegBootLightBoxLink", function(e) {
     var $ThisLink = $(this).attr('href');
-    $("#PegBootLightBox").css('background-image', 'url(' + $ThisLink + ')');
-    /* Begin Add Headline */
-    var $picParent = $(this).closest("div.product-row");
-    var $picHeadline = $picParent.find("h2");
-    $("#PegBootLightBoxHeadline").text($picHeadline.text());
-    /* End Add Headline */
-    $("#PegBootLightBox").fadeIn($FadeSpeed);
-    e.preventDefault();
+    var $ThisLinkType = $ThisLink.substr($ThisLink.lastIndexOf('.')+1 );
+    $ThisLinkType = $ThisLinkType.toLowerCase();
+    switch($ThisLinkType) {
+      case 'gif':
+      case 'jpg':
+      case 'png':
+        $("#PegBootLightBox").css('background-image', 'url(' + $ThisLink + ')');
+        /* Begin Add Headline */
+        var $picParent = $(this).closest("div.product-row");
+        var $picHeadline = $picParent.find("h2");
+        $("#PegBootLightBoxHeadline").text($picHeadline.text());
+        /* End Add Headline */
+        $("#PegBootLightBox").fadeIn($FadeSpeed);
+        e.preventDefault();
+      break;
+    }
   });
   /* End code to find & show the lightbox + image when clicked */
-
-  /* Begin Close the lightbox area when clicked. */
-  $("#FullPageContainer").on("click", "#PegBootLightBox", function() {
-    $("#PegBootLightBox").fadeOut($FadeSpeed);
-  });
-  /* End Close the lightbox area when clicked. */
-</script>
